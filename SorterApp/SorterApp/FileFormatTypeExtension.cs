63e607ca007754f2fcea420204e5c8b1fea87a21
itskhawarizmi <itskhawarizmi@gmail.com>
2019-08-31 14:49:37 +0700

    Adding file format type extension

---
+++ SorterApp/SorterApp/FileFormatTypeExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace SorterApp
+{
+    /// <summary>
+    /// The file format type extension. Set the format of file
+    /// with specific format.
+    /// </summary>
+    public enum FileFormatTypeExtension
+    {
+        /// <summary>
+        /// There's not file format specified.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Sets the file format to .txt.
+        /// </summary>
+        Text = 1,
+
+        /// <summary>
+        /// Sets the file format to .doc.
+        /// </summary>
+        Document = 2,
+
+        /// <summary>
+        /// Sets the file format to .pdf.
+        /// </summary>
+        Pdf = 3,
+
+    }
+}
